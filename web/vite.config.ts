1c7b44a29a6e628963382ef10a9032fc02c9507a
iaoongin <28687951+iaoongin@users.noreply.github.com>
2025-03-24 14:35:29 +0800

    ci: 调整部署方案

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,37 +1,26 @@
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import serveStatic from "serve-static";
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(),
+  plugins: [
+    react(),
+    tsconfigPaths(),
     {
       name: "multi-public-dirs",
       configureServer(server) {
         // 添加其他目录为静态资源
         server.middlewares.use(
-          '/data',
-          serveStatic("../spider/data", { etag: true }),
-        )
+          "/data",
+          serveStatic("../spider/data", { etag: true })
+        );
         server.middlewares.use(
-          '/img',
-          serveStatic("../spider/img", { etag: true }),
-        )
-      }
+          "/img",
+          serveStatic("../spider/img", { etag: true })
+        );
+      },
     },
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../spider/data/**/*',  // 源目录（支持通配符）
-          dest: 'data'                 // 目标目录（相对于 dist）
-        },
-        {
-          src: '../spider/img/**/*',
-          dest: 'img'
-        }
-      ]
-    })
   ],
-})
+});
