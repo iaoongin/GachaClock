422585c7183a5ff63ed6dd618cf71351e342d0a5
iaoongin <28687951+iaoongin@users.noreply.github.com>
2025-07-23 18:05:34 +0800

    fix(spider): 修正 sr 爬取结果，排除了一些特殊的卡池
    - 更新了 meta.json 中 sr 数据的文件路径
    - 新增了 20250723120000_20250812150059.json 文件，包含新的角色和武器跃迁数据
    - 修改了 sr.py 中的爬虫逻辑，排除了一些特殊的卡池

--- spider/spider/spiders/sr.py
+++ spider/spider/spiders/sr.py
@@ -12,31 +12,42 @@ class SrSpider(scrapy.Spider):
             "spider.pipelines.SpiderPipeline": 300,
         },
     }
+
     def parse(self, response):
-        raw_gacha_list = response.xpath('//*[@id="mw-content-text"]/div/div[2]/div[3]/div[1]/div/div')
+        raw_gacha_list = response.xpath(
+            '//*[@id="mw-content-text"]/div/div[2]/div[3]/div[1]/div/div'
+        )
 
         for gacha in raw_gacha_list:
             # banner_title
-            banner_title = gacha.xpath('./a//b/text()').extract_first()
-            if banner_title == None or "跃迁" not in banner_title:
+            banner_title = gacha.xpath("./a//b/text()").extract_first()
+            if (
+                banner_title == None
+                or "跃迁" not in banner_title
+                # 排除一些特殊的卡池
+                or "圣剑辉烈之誓" in banner_title
+                or "此身剑所天成" in banner_title
+                or "王的诞生" in banner_title
+                or "冬木赤影" in banner_title
+            ):
                 continue
-            
+
             # gacha
             g = []
-            g_title = gacha.xpath('./p//img/@alt').extract()
-            g_img = gacha.xpath('./p//img/@src').extract()
+            g_title = gacha.xpath("./p//img/@alt").extract()
+            g_img = gacha.xpath("./p//img/@src").extract()
             for i in range(len(g_title)):
-                g.append({
-                    "title": g_title[i],
-                    "img": g_img[i].replace("56", "112")
-                })
-            
+                g.append({"title": g_title[i], "img": g_img[i].replace("56", "112")})
+
             # time
-            eventTimer = [gacha.xpath('./div/span/@data-start').extract_first()+':00', gacha.xpath('./div/span/@data-end').extract_first()+':59']
-            
+            eventTimer = [
+                gacha.xpath("./div/span/@data-start").extract_first() + ":00",
+                gacha.xpath("./div/span/@data-end").extract_first() + ":59",
+            ]
+
             item = SpiderItem()
             item["title"] = banner_title
-            item["type"] =  '角色' if '角色' in banner_title else '武器'
+            item["type"] = "角色" if "角色" in banner_title else "武器"
             item["timer"] = eventTimer
             item["gachas"] = g
             yield item
